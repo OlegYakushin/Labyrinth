3db5b2d51c3c5896eb02a5aa42e91b1973ebe497
Oleg <yakushinoa@gmail.com>
2024-04-04 13:33:42 +0200

    Initial Commit

---
+++ Labyrinth/LabyrinthApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabyrinthApp.swift
+//  Labyrinth
+//
+//  Created by Oleg Yakushin on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct LabyrinthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
